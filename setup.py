9712a196799153448049bf532b4ea78cc0b5e714
Adam Cherti <adam@cherti.name>
2016-11-16 17:32:10 -0500

    add setup.py and requirements file in order to package the app

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+        name = "django-gcalsync",
+        packages=find_packages(),
+        version = "0.1.0",
+        description = "A Django application to sync with GCal.",
+        author = "Jonathon Morgan",
+        author_email = "jonathon@newknowledge.com"
+        url = "https://github.com/gati",
+        classifiers = [
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Internet :: WWW/HTTP',
+        ],
+        install_requires = [
+            'rfc3339==5',
+            'django-model-utils==2.0.3', 
+            'tzlocal==1.1.1', 
+            'celery==3.1.11'
+            ]
+     )
